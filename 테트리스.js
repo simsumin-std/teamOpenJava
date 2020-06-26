a65d921f7c7b680bb736e42f6903ac406ce07ed1
justuser8 <thsxotn1@naver.com>
2020-06-26 22:28:02 +0900

    다시 만드는 테트리스

--- 테트리스.js
+++ 테트리스.js
@@ -1,18 +1,45 @@
-var tetris= document.querySelector('#tetris');
-var tetrisData=[];
+var init=false;
+    var myCanvas;
+    var Context;
 
-function 칸만들기(){ // 표 만드는 함수
-	var fragment=document.createDocumentFragment();
-	for(var i=0; i<20; i++){
-		var tr= document.createElement('tr');
-		fragment.appendChild(tr);
-		for (var j=0; j<10; j++){
-			var td=document.createElement('td');
-			tr.appendChild(td);
-		}
+    function Init()
+    {
+        if(init==flase)
+        {
+            myCanvas=document.getElementById("MyCanvas")
+            Context=myCavas.getContext("2d");
+            init=ture;
+            
+        }
+    }
+
+    function Run()
+    {
+        onDraw();
+    }
+
+    function onDraw()
+    {
+        if(init==false)return;
+
+        Context.strokeStyle="#000"; //색상
+        Context.lineWidth=1; //선두께
+        Context.strokeRect(0, 0, myCanvas.width-1,myCanvas.height-1); //테두리만 있는 사각형
+    }
+$(document).ready(function(){ //자동 실행 함수
+    Init();
+    setInterval(Run,100); //내부 함수 주기적으로 실행
+});
+
+var square;
+
+function square_init()
+{
+	suqre=new Array();
+	for(i=0;i<20;++i) //전위 연산자
+	{
+		square.push(new Array(10));
+		for(j=0;j<10;++j)
+		square[i][j]=0; //배열 초기화
 	}
-	console.log(tetris,fragment);
-	tetris.appendChild(fragment);
 }
-
-칸만들기();
